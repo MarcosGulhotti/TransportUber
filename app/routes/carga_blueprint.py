a34b803aa32e7624a35023a2f2d8f31801a727f2
Stefano Garrigó Ludvichak <stefanogl@gmail.com>
2021-12-08 12:46:45 -0300

    feat: rota deletar carga implementada

--- app/routes/carga_blueprint.py
+++ app/routes/carga_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from app.controllers.carga_controller import listar_carga
+from app.controllers.carga_controller import deletar_carga, listar_carga
 
 bp_carga = Blueprint('bp_carga', __name__, url_prefix='/carga')
 
 bp_carga.get('/<int:carga_id>')(listar_carga)
+bp_carga.delete('/<int:carga_id>')(deletar_carga)
