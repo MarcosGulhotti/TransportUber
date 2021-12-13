08c82d0b34fe7dae841f4349bd2e77aa5647b6b3
marcos <marcosmvg77@gmail.com>
2021-12-13 10:32:14 -0300

    fix: merge request

--- app/controllers/caminhao_controller.py
+++ app/controllers/caminhao_controller.py
@@ -13,7 +13,6 @@ def criar_caminhao():
   data['marca'] = data['marca'].title()
   data['modelo'] = data['modelo'].title()
   data['motorista_id'] = current_user
-  print(current_user)
 
   novo_caminhao = CaminhaoModel(**data)
 
